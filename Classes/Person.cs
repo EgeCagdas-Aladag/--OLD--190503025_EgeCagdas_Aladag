8c3bbcfb57b72f771ca10765f636b1a3ad19dc9e
EgeCagdas-Aladag <egecagdasaladag01@hotmail.com>
2022-04-06 19:42:27 +0300

    Testing

---
+++ Classes/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Musikschule_Verwaltungsanwendung
+{
+    class Person
+    {
+        protected int id;
+        protected string name;
+        protected string surname;
+
+    //Constructors
+        public Person(int id, string name, string surname)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+
+        }
+
+        public Person(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+    //Functions:
+        //Get&Set Functions
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        //Others
+
+    }
+}
---
+++ Classes/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Musikschule_Verwaltungsanwendung.Classes
+{
+    class Teacher : Person
+    {
+        private int teacherId;
+        private List<int> offeredCourses = new List<int>();
+        private DateTime workStart;
+
+    //Constructors 
+        
+
+        public Teacher(int id, string name, string surname)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+    }
+}
