a7285bd2f36a0add212524cd191c44c5fa265c5c
Ege Cagdas Aladag <egecagdasaladag01@hotmail.com>
2022-04-10 01:43:03 +0300

    add first SQLite connection
    Create database under .\Data to be used as primary database for the software

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+
 
 namespace Private_Musikschule_Verwaltungsanwendung
 {
@@ -15,7 +17,36 @@ namespace Private_Musikschule_Verwaltungsanwendung
         public Form1()
         {
             InitializeComponent();
+
+            
+
+            
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            SQLiteConnection sqlite_connection;
+            sqlite_connection = CreateConnection();
+        }
+        
+        SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_connection;
+            sqlite_connection = new SQLiteConnection(@"DataSource=D:\INF202 Project\Private Musikschule Verwaltungsanwendung\Private Musikschule Verwaltungsanwendung\Data\MusicSchool.db;" +
+                "Version=3;");
+            try
+            {
+                sqlite_connection.Open();
+            }
+            catch (Exception e)
+            {
+                //TODO: Catch exception
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("CONNECTED TO DATABASE.");
+            return sqlite_connection;
+        }
     }
 }
